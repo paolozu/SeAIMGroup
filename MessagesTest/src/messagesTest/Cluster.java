f43aee44959074006a888f74698490ec708b7bb8
StefanoMartella <stefano.martella9614@gmail.com>
2017-12-16 17:01:20 +0100

    Implementing clusters and robots

--- MessagesTest/src/messagesTest/Cluster.java
+++ MessagesTest/src/messagesTest/Cluster.java
@@ -1,85 +1,92 @@
 package messagesTest;
 
-import java.util.Collection;
+import java.sql.Timestamp;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Cluster {
 	
-	private Map<Integer,Integer> robots_IR;
-	private int cluster_IR;
+	private Map<Integer, Robot> robots_IR;   // Map in which the key is robot_id and the value is robot_IR.
+	private int down_robots; 	              // Counter of down robots in this cluster.
+	private double cluster_IR;
 	private int cluster_id;
 	private int area_id;
-	private int downtime;
+	private Timestamp start_downtime;         // To keep trace when down time starts.
+	private long total_downtime;			  // Total down time in milliseconds.
 	
 	public Cluster(){
-		robots_IR = new HashMap<Integer,Integer>();
-	}
-
-	public Map<Integer, Integer> getRobotsIR() {
-		return robots_IR;
-	}
-
-	public void setRobotsIR(Map<Integer, Integer> robots_IR) {
-		this.robots_IR = robots_IR;
+		robots_IR = new HashMap<Integer, Robot>(); 
 	}
 	
-	public int getClusterIR() {
-		return cluster_IR;
-	}
+	// Getters and Setters
+	
+	public Map<Integer, Robot> getRobotsIR() { return robots_IR; }
+	public int getDownRobots() { return this.down_robots; }
+	public double getClusterIR() { return cluster_IR; }
+	public int getClusterid() { return cluster_id; }
+	public int getAreaId() { return area_id; }
+	public long getDowntime() { return total_downtime; }
+	public Timestamp getStartDowntime() { return start_downtime; }
 
-	public void setClusterIR(int cluster_IR) {
-		this.cluster_IR = cluster_IR;
-	}
-
-	public int getClusterId() {
-		return cluster_id;
-	}
-
-	public void setClusterId(int cluster_id) {
-		this.cluster_id = cluster_id;
-	}
-
-	public int getAreaId() {
-		return area_id;
-	}
-
-	public void setAreaId(int area_id) {
-		this.area_id = area_id;
-	}
-
-	public int getDowntime() {
-		return downtime;
-	}
-
-	public void setDowntime(int downtime) {
-		this.downtime = downtime;
-	}
+	public void setRobotsIR(Map<Integer, Robot> robots_IR) { this.robots_IR = robots_IR; }
+	public void setDownRobots(int down_robots) { this.down_robots = down_robots; }
+	public void setClusterIR(int cluster_IR) { this.cluster_IR = cluster_IR; }
+	public void setClusterId(int cluster_id) { this.cluster_id = cluster_id; }
+	public void setAreaId(int area_id) { this.area_id = area_id; }
+	public void setDowntime(long total_downtime) { this.total_downtime = total_downtime; }
+	public void setStartDowntime(Timestamp start_downtime) { this.start_downtime = start_downtime; }
+	
+	// Other methods
+	
 	public void addRobot(Robot robot){
-		robots_IR.put(robot.getRobotId(), robot.getDownSignals());
-	}
-	
-	public void updateDownTime(){
-		
-		Collection<Integer> temp = this.robots_IR.values();
-		
-		for (int x : temp){
-			if ( x != 0 ){
-				if ( this.downtime < 60 )
-					 this.downtime += 1;
-			}
-			else{
-				if ( this.downtime > 0 )
-					 this.downtime -= 1;
-			}
+		if ( this.robots_IR.containsKey(robot.getRobotId()) ) {
+			this.updateRobotIR(robot);
+		}
+		else {
+			// Here we insert new robot in this cluster.
+			// Here the new robot is no still present
+			// so the first message of the new robot needs to be a down signal
+			// so we increase the down_robots counter for this cluster.
+			this.robots_IR.put(robot.getRobotId(), robot);
+			this.down_robots++;
 		}
 	}
 	
-	public void updateIR(){
-		this.cluster_IR = this.downtime / 60 * 100;
-	}
+	public void updateRobotIR(Robot robot) {
+		
+		switch( robot.getDownSignals() ) {
 
-	public Map<Integer, Integer> getRobots_state() {
-		return robots_IR;
+			case 0:		this.robots_IR.put(robot.getRobotId(), robot);
+						if( --this.down_robots == 0 )
+							this.updateDownTime();
+						break;
+					
+			case 1:		if( robot.getPreviuosDownSignals() != 2 ) {
+							this.down_robots++;
+							this.robots_IR.put(robot.getRobotId(), robot);
+							this.start_downtime = new Timestamp(System.currentTimeMillis());
+							break;
+						}
+					
+			default:	this.robots_IR.put(robot.getRobotId(), robot);
+		
+		}
+	}
+	
+	public void updateDownTime(){
+		this.total_downtime += new Timestamp(System.currentTimeMillis()).getTime() - start_downtime.getTime();
+		this.updateIR();
+	}
+	
+	public void updateIR(){
+		// total_downtime --> milliseconds
+		// * 1.6667e-5    --> minutes
+		// divide to 60   --> IR not in percentage
+		// divide to 60   --> IR in percentage
+		this.cluster_IR = (double)((total_downtime * 1.6667e-5) / 60) * 100;
+	}
+	
+	public double forceUpdateIR() {
+		return 2.0;
 	}
 }
--- MessagesTest/src/messagesTest/Robot.java
+++ MessagesTest/src/messagesTest/Robot.java
@@ -6,10 +6,11 @@ public class Robot {
 	
 	private int robot_id;
 	private int cluster_id;
-	private int down_signals;
+	private int previous_down_signals;		// To keep trace of down signals evolution.
+	private int down_signals;				// To keep trace of down signals numbers.
 	private double robot_IR;
-	private Timestamp start_downtime;
-	private long total_downtime;
+	private Timestamp start_downtime;		// To keep trace when down time starts.
+	private long total_downtime;			// Total down time in milliseconds.
 	
 	public Robot() {}
 	
@@ -17,42 +18,45 @@ public class Robot {
 		this.robot_id = robot_id;
 		this.cluster_id = cluster_id;
 	}
-	
+		
 	// Getters and Setters
-	
+		
 	public int getRobotId() { return robot_id; }
 	public int getClusterId() { return cluster_id; }
 	public int getDownSignals() { return down_signals; }
+	public int getPreviuosDownSignals() { return this.previous_down_signals; }
 	public long getDowntime() { return total_downtime; }
-	public Timestamp getStart_downtime() { return start_downtime; }
+	public Timestamp getStartDowntime() { return start_downtime; }
 	public double getRobotIR() { return robot_IR; }
 	
 	public void setRobotId(int robot_id) { this.robot_id = robot_id; }
 	public void setClusterId(int cluster_id) { this.cluster_id = cluster_id; }
 	public void setDownSignals(int signal_state) { this.down_signals = signal_state; }
+	public void setPreviousDownSignal(int previous_down_signals) { this.previous_down_signals = previous_down_signals; }
 	public void setDowntime(long total_downtime) { this.total_downtime = total_downtime; }
-	public void setStart_downtime(Timestamp start_downtime) { this.start_downtime = start_downtime; }
+	public void setStartDowntime(Timestamp start_downtime) { this.start_downtime = start_downtime; }
 	public void setRobotIR(int robot_IR) { this.robot_IR = robot_IR; }
 	
 	// Other methods
 	
+	// Function to update robots according to the received message.
+	// In case the number of down signals became 0 we update total down time.
+	// In case the number of down signals passes from 0 to 1 the we store
+	// the current time to keep trace of down time beginning.
 	public void signalCatch(int signal) {
 		if ( signal == 1 ){ 
-			if ( down_signals > 0 ) {
-				this.down_signals -= 1;
-				if( down_signals == 0 )
-					this.updateDownTime(this.start_downtime);
-			}
+			this.previous_down_signals = this.down_signals;
+			if( --down_signals == 0 )
+				this.updateDownTime();
 		}
 		else { // signal == 0 
-			this.down_signals += 1;
-			if( this.down_signals == 1 ) { 
+			this.previous_down_signals = this.down_signals;
+			if( ++this.down_signals == 1 ) 
 				this.start_downtime = new Timestamp(System.currentTimeMillis());
-			}
 		}	
 	}
 	
-	public void updateDownTime(Timestamp time) {
+	public void updateDownTime() {
 		this.total_downtime += new Timestamp(System.currentTimeMillis()).getTime() - start_downtime.getTime();
 		this.updateIR();
 	}
@@ -65,6 +69,16 @@ public class Robot {
  		this.robot_IR = (double)((total_downtime * 1.6667e-5) / 60) * 100;
 	}
 	
+	// Function to force IR update in case we need current IR and the down_signals is greater than 0.
+	// We need this function because otherwise we update total_downtime only when down_signals counter
+	// returns to be 0.
+	public double forceUpdateIR(Timestamp time) {
+		if ( down_signals > 0 ) {
+			this.updateDownTime();
+			this.start_downtime = new Timestamp(System.currentTimeMillis());
+		}
+		return this.robot_IR;
+	}
 	
 	public String toString() {
 		return( this.robot_id + " " + this.cluster_id );
