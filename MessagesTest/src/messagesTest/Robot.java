f4a8a6c873b9d8d16e2994d635eb4c12a030805d
StefanoMartella <stefano.martella9614@gmail.com>
2017-12-17 23:12:52 +0100

    Implementing robots and clusters

--- MessagesTest/src/messagesTest/Robot.java
+++ MessagesTest/src/messagesTest/Robot.java
@@ -1,16 +1,19 @@
 package messagesTest;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Robot {
 	
 	private int robot_id;
 	private int cluster_id;
-	private int previous_down_signals;		// To keep trace of down signals evolution.
-	private int down_signals;				// To keep trace of down signals numbers.
+	private int previous_down_signals;						// To keep trace of down signals evolution.
+	private int down_signals;								// To keep trace of down signals numbers.
 	private double robot_IR;
-	private Timestamp start_downtime;		// To keep trace when down time starts.
-	private long total_downtime;			// Total down time in milliseconds.
+	private Timestamp start_downtime;						// To keep trace when down time starts.
+	private TreeMap<Timestamp, Long> downtime_intervals;
 	
 	public Robot() {}
 	
@@ -21,21 +24,21 @@ public class Robot {
 		
 	// Getters and Setters
 		
-	public int getRobotId() { return robot_id; }
-	public int getClusterId() { return cluster_id; }
-	public int getDownSignals() { return down_signals; }
+	public int getRobotId() { return this.robot_id; }
+	public int getClusterId() { return this.cluster_id; }
+	public int getDownSignals() { return this.down_signals; }
 	public int getPreviuosDownSignals() { return this.previous_down_signals; }
-	public long getDowntime() { return total_downtime; }
-	public Timestamp getStartDowntime() { return start_downtime; }
-	public double getRobotIR() { return robot_IR; }
+	public Timestamp getStartDowntime() { return this.start_downtime; }
+	public double getRobotIR() { return this.robot_IR; }
+	public TreeMap<Timestamp, Long> getDowntimeIntervals() { return this.downtime_intervals; }
 	
 	public void setRobotId(int robot_id) { this.robot_id = robot_id; }
 	public void setClusterId(int cluster_id) { this.cluster_id = cluster_id; }
 	public void setDownSignals(int signal_state) { this.down_signals = signal_state; }
 	public void setPreviousDownSignal(int previous_down_signals) { this.previous_down_signals = previous_down_signals; }
-	public void setDowntime(long total_downtime) { this.total_downtime = total_downtime; }
 	public void setStartDowntime(Timestamp start_downtime) { this.start_downtime = start_downtime; }
 	public void setRobotIR(int robot_IR) { this.robot_IR = robot_IR; }
+	public void setDowntimeIntervals(TreeMap<Timestamp, Long> downtime_intervals) { this.downtime_intervals = downtime_intervals; }
 	
 	// Other methods
 	
@@ -44,29 +47,56 @@ public class Robot {
 	// In case the number of down signals passes from 0 to 1 the we store
 	// the current time to keep trace of down time beginning.
 	public void signalCatch(int signal) {
-		if ( signal == 1 ){ 
+		if ( signal == 0 ){ 
+			this.previous_down_signals = this.down_signals;
+			if( ++this.down_signals == 1 ) 
+				this.start_downtime = new Timestamp(System.currentTimeMillis());	
+		}
+		else { // signal == 1
 			this.previous_down_signals = this.down_signals;
 			if( --down_signals == 0 )
 				this.updateDownTime();
-		}
-		else { // signal == 0 
-			this.previous_down_signals = this.down_signals;
-			if( ++this.down_signals == 1 ) 
-				this.start_downtime = new Timestamp(System.currentTimeMillis());
 		}	
 	}
 	
 	public void updateDownTime() {
-		this.total_downtime += new Timestamp(System.currentTimeMillis()).getTime() - start_downtime.getTime();
+		long downtime_duration = new Timestamp(System.currentTimeMillis()).getTime() - start_downtime.getTime();
+		this.downtime_intervals.put(start_downtime, downtime_duration);
 		this.updateIR();
 	}
 
 	public void updateIR() {
+		
+		long downtime_last_hour = 0;
+		ArrayList<Timestamp> more_than_an_hour_ago = new ArrayList<>();
+			
+		for( Map.Entry<Timestamp, Long> interval : downtime_intervals.entrySet() ) {
+			
+			long time_to_downtime_init = new Timestamp(System.currentTimeMillis()).getTime() - interval.getKey().getTime();
+			
+			// 3.6e6 milliseconds --> 1 hour.
+			if( time_to_downtime_init > 3.6e6 ) {
+				if( time_to_downtime_init - interval.getValue() > 3.6e6  ) {
+					more_than_an_hour_ago.add(interval.getKey());		
+				}
+				else {
+					downtime_last_hour += (3.6e6 - time_to_downtime_init + interval.getValue());
+				}
+			}
+			else {
+				downtime_last_hour += interval.getValue();
+			}
+			
+		}
+		
+		for( Timestamp invalid : more_than_an_hour_ago ) {
+			this.downtime_intervals.remove(invalid);
+		}
 		// total_downtime --> milliseconds
 		// * 1.6667e-5    --> minutes
 		// divide to 60   --> IR not in percentage
 		// divide to 60   --> IR in percentage
- 		this.robot_IR = (double)((total_downtime * 1.6667e-5) / 60) * 100;
+ 		this.robot_IR = (double)((downtime_last_hour * 1.6667e-5) / 60) * 100;
 	}
 	
 	// Function to force IR update in case we need current IR and the down_signals is greater than 0.
@@ -80,7 +110,9 @@ public class Robot {
 		return this.robot_IR;
 	}
 	
+	@Override
 	public String toString() {
-		return( this.robot_id + " " + this.cluster_id );
+		return( "Robot ID: " + this.robot_id + "\nCluster ID: " + this.cluster_id +
+				"\nDown signals " + this.down_signals + "\nRobot IR: " + this.robot_IR );
 	}
 }
