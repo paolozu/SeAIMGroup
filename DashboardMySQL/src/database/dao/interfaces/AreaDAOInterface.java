853c563d414254b1f2deaeaac317ee1b1a42c309
StefanoMartella <stefano.martella9614@gmail.com>
2018-01-19 09:04:40 +0100

    Minor fix

--- DashboardMySQL/src/database/dao/interfaces/AreaDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package database.dao.interfaces;
-
-import java.util.ArrayList;
-import applicationCore.Cluster;
-
-public interface AreaDAOInterface {
-	
-	public ArrayList<Cluster> getClusters(Integer area_id);
-}
--- DashboardMySQL/src/database/dao/interfaces/RobotDAOInterface.java
+++
@@ -1,9 +0,0 @@
-package database.dao.interfaces;
-
-import applicationCore.Robot;
-
-public interface RobotDAOInterface {
-	
-	public void insertRobot(Robot robot);
-	public void updateRobot(Robot robot);
-}
